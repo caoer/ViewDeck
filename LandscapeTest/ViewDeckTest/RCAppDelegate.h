1547724e46adf874955d30e0ad0f8150f183d144
Zitao Xiong <caoer115@gmail.com>
2013-05-22 19:58:14 -0500

    add landscape test

---
+++ LandscapeTest/ViewDeckTest/RCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCAppDelegate.h
+//  ViewDeckTest
+//
+//  Created by Zitao Xiong on 5/22/13.
+//  Copyright (c) 2013 nanaimostudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LandscapeTest/ViewDeckTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewDeckTest
+//
+//  Created by Zitao Xiong on 5/22/13.
+//  Copyright (c) 2013 nanaimostudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCAppDelegate class]));
+    }
+}
